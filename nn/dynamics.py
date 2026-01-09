34a10a8405760bd7e1e14236693d8f3548bceb7d
ayesha <ayeshavermani@gmail.com>
2026-01-09 14:30:12 +0530

    updated dynamics and likelihood

---
+++ nn/dynamics.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+EPS = 1e-6
+
+
+class LoRAHypernet(nn.Module):
+    """
+    Hypernetwork that outputs low-rank updates for specified weights
+    """
+    def __init__(
+            self,
+            embedding_dim: int,
+            layer_dims: list[tuple[int, int]],
+            adapt_layers: set[int] | None = {0, 1},
+            rank: int = 1,
+            hidden_dim: int = 256,
+            ):
+        super().__init__()
+
+        self.rank = rank
+        self.adapt_layers = set(range(len(layer_dims))) if adapt_layers is None else adapt_layers
+
+        self.net = nn.Sequential(nn.Linear(embedding_dim, hidden_dim),
+                                 nn.Tanh(),
+                                 nn.Linear(hidden_dim, hidden_dim),
+                                 nn.Tanh())
+        
+        self.heads = nn.ModuleList()
+
+        for dim_in, dim_out in layer_dims:
+            self.heads.append(nn.Linear(hidden_dim, rank * dim_in + rank * dim_out))
+        
+    def set_adapt_layers(self, adapt_layers: set[int] | None):
+        """Change which layers are adapted at runtime"""
+        self.adapt_layers = set(range(len(self.layer_dims))) if adapt_layers is None else set(adapt_layers)
+
+    def forward(self, e) -> tuple[dict[str, torch.Tensor], float]:
+        if e.dim() == 1:
+            e = e.unsqueeze(0)
+        B = e.shape[0]
+
+        out = self.net(e)
+
+        deltas = {}
+        delta_norm = 0
+
+        for i, (dim_in, dim_out) in enumerate(self.layer_dims):
+            if i not in self.adapt_layers:
+                continue
+            
+            param_vec = self.heads[i](out)
+
+            u, vh = torch.split(param_vec, [dim_in * self.rank, dim_out * self.rank], -1)
+
+            u = u.view(B, dim_in, self.rank)
+            vh = vh.view(B, dim_out, self.rank)
+
+            delta = torch.einsum('bij, bkj -> bik', [u, vh]).squeeze()
+
+            deltas[f"linear_{i}"] = delta
+            delta_norm += torch.norm(delta, (-1, -2))
+
+        return deltas, delta_norm
+
+
+class MlpDynamics(nn.Module):
+    def __init__(
+            self,
+            num_latents: int,
+            hidden_layers: int = 2,
+            dim: int = 128,
+            adapt_layers: set[int] | None = {0, 1}
+            ):
+        super().__init__()
+
+        dims = [num_latents] + hidden_layers * [dim] + [num_latents]
+
+        self.adapt_layers = adapt_layers
+
+        self.fc_layers = nn.ModuleList(nn.Linear(dims[i], dims[i+1]) for i in range(len(dims) - 1))
+        
+        self.act = nn.Tanh()
+        self.log_variance = nn.Parameter(torch.ones(1, num_latents))
+
+    def forward(self, z_prev, deltas = None):
+
+        z_next = z_prev
+        for i, linear in enumerate(self.fc_layers):
+            z_next = linear(z_next)
+
+            if deltas is not None and i in self.adapt_layers:
+                z_next = z_next + z_next @ deltas[f"linear_{i}"]
+
+            if i < len(self.fc_layers) - 1:
+                z_next = self.act(z_next)
+
+        variance = F.softplus(self.log_variance) + EPS
+        return z_next, variance
+
+
+
+
---
+++ nn/likelihood.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.distributions import Normal, Poisson
+
+EPS = 1e-6
+
+
+class Likelihood(nn.Module):
+    def __init__(
+            self,
+            num_latents : int,
+            num_observations: int,
+            linear: bool = True,
+            dim_hidden: int = 128
+        ):
+        super().__init__()
+
+        if linear:
+            self.readout = nn.Linear(num_latents, num_observations)
+        else:
+            self.readout = nn.Sequential(
+                nn.Linear(num_latents, dim_hidden),
+                nn.SiLU(),
+                nn.Linear(dim_hidden, num_observations)
+            )
+
+    def get_mean_output(self, z):
+        return self.readout(z)
+
+    def forward(self, z, y):
+        pass
+
+
+class GaussianLikelihood(Likelihood):
+    def __init__(
+            self,
+            num_latents : int,
+            num_observations: int,
+            linear: bool = True,
+            dim_hidden: int = 128
+    ):
+        super().__init__(num_latents, num_observations, linear, dim_hidden)
+
+        self.log_variance = nn.Parameter(torch.ones(1, num_observations))
+
+    def forward(self, z, y):
+        mu = self.get_mean_output(z)
+        variance = F.softplus(self.log_variance) + EPS
+
+        log_likelihood = Normal(mu, torch.sqrt(variance)).log_prob(y)
+
+        return torch.sum(log_likelihood, (-1, -2))
+
+    
+class PoissonLikelihood(Likelihood):
+    def __init__(
+            self,
+            num_latents : int,
+            num_observations: int,
+            linear: bool = True,
+            dim_hidden: int = 128
+    ):
+        super().__init__(num_latents, num_observations, linear, dim_hidden)
+
+    def forward(self, z, y):
+        log_rates = self.get_mean_output(z)
+        rates = F.softplus(log_rates) + EPS
+
+        log_likelihood = Poisson(rates).log_prob(y)
+
+        return torch.sum(log_likelihood, (-1, -2))
+
+
+
+
+        
+
